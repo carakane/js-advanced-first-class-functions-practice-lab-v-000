048e5ba83318ea6c9e58909d8bc33b6469bd17d3
Cara Kane <cara.kane@gmail.com>
2017-11-10 01:23:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function logDriversByHometown(drivers, location) {
 
 function driversByRevenue(drivers) {
    drivers.sort(function (a, b) {
-    return a - b
+    return a.revenue - b.revenue
   })
 }
